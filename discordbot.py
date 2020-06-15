1fe15aa45d88ccadc6337c850a365f9472b748e0
OSENBRAIN <66958328+OSENBRAIN@users.noreply.github.com>
2020-06-15 23:54:13 +0900

    main.py

--- discordbot.py
+++ discordbot.py
@@ -63,5 +63,4 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
         # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
         await msg.remove_reaction(str(reaction.emoji), user)
 
-
 client.run('NzIyMDQ2OTg3NjE2MTI0OTY4.Xud0LA.yTvXHSRrnf7eFKGtwt3rtM-s0Ts')
